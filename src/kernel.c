72afb7f72f64c35ad3a61b58c67ce44960da550d
armadilloNik <nik@tumpedover.com>
2017-08-01 23:46:08 -0500

    simple and quite presumptuous build script

--- src/kernel.c
+++ src/kernel.c
@@ -5,6 +5,9 @@
 #endif
 #include <stddef.h>
 #include <stdint.h>
+
+
+#include "multiboot.h"
  
 /* Check if the compiler thinks we are targeting the wrong operating system. */
 #if defined(__linux__)
@@ -108,10 +111,39 @@ void terminal_writestring(const char* data) {
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kernel_main. */
 #endif
-void kernel_main(void) {
+
+
+
+typedef struct multiboot_memory_map {
+	unsigned int size;
+	unsigned int base_addr_low,base_addr_high;
+	unsigned int length_low,length_high;
+	unsigned int type;
+} multiboot_memory_map_t;
+
+void kernel_main(multiboot_info_t* mbt) {
+
+multiboot_memory_map_t* mmap = mbt->mmap_addr;
+    
+    char aux[10];// = "hello ";
+	//aux = "This is Grove OS\nWelcome to the Stedding\nMemSize: %u";
+	
+    aux[0] = '1';
+    aux[1] = '2';
+
+    terminal_initialize();
+
+    terminal_writestring("Checking the memMap\n");
+	while(mmap < mbt->mmap_addr + mbt->mmap_length) {
+		
+		mmap = (multiboot_memory_map_t*) ( (unsigned int)mmap + mmap->size + sizeof(mmap->size) );
+		//totalSize += mmap->size;
+	}
+
 	/* Initialize terminal interface */
-	terminal_initialize();
+	
  
 	/* Newline support is left as an exercise. */
-	terminal_writestring("This is Grove OS\nWelcome to the Stedding");
+	terminal_writestring("memSize\n");
+	terminal_writestring(aux);
 }
